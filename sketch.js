47dcadc8bf90faeb18732f12abc124b676fa090d
emmmali <el36@rice.edu>
2021-09-18 17:34:10 -0500

    Working integration of FDA API (definition of drug)

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
 function setup() {
     noCanvas();
     
+
     let bgpage = chrome.extension.getBackgroundPage();
-    drug = bgpage.drug;
-    def = "The drug def"
-    createP("Drug: " + drug).style("font-size", "20pt")
-    createP(drug);
+    drug_input = bgpage.drug;
+
+    let url_1 = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+    let url = url_1 + drug_input;
+    
+    loadJSON(url, gotData);
+
+    function gotData(data) {
+        createP(data[0].meanings[0].definitions[0].definition);
+    }
+    //def = "The drug def"
+    createP("Drug: " + drug_input).style("font-size", "20pt")
+    //createP(drug_input);
+    
 }
