d90d183768cd5c0182ad09e947adb0e13e91cf3a
Janhvi Somaiya <js212@rice.edu>
2021-09-18 18:27:19 -0500

    Fully working FDA API implementation, need to work on UI/UX

--- sketch.js
+++ sketch.js
@@ -5,16 +5,33 @@ function setup() {
     let bgpage = chrome.extension.getBackgroundPage();
     drug_input = bgpage.drug;
 
-    let url_1 = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-    let url = url_1 + drug_input;
+    let url_1 = 'https://api.fda.gov/drug/event.json?search=(receivedate:[20040101+TO+20210918])+AND+'; 
+    let url = url_1 + drug_input.trim() + '&count=patient.drug.drugindication.exact';
     
     loadJSON(url, gotData);
+    
 
     function gotData(data) {
-        createP(data[0].meanings[0].definitions[0].definition);
+        var drug_uses = "";
+        var indx = 0;
+        var counter = 0;
+
+        while (counter < 5 && indx < data.results.length){
+            value = data.results[indx].term;
+            if (value != "PRODUCT USED FOR UNKNOWN INDICATION" &&
+            value != "DRUG USED FOR UNKNOWN INDICATION"){
+                drug_uses += (value + "\n");
+                counter++;
+            }
+            indx++;
+        }
+
+        createP(drug_uses);
     }
     //def = "The drug def"
     createP("Drug: " + drug_input).style("font-size", "20pt")
+    def = "The drug is used for:";
+    createP(def);
     //createP(drug_input);
     
 }
